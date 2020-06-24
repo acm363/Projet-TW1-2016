f0b312569e6eb09d9b374979d2b0291310f70600
acm363 <atchedjichurchill@gmail.com>
2020-06-24 18:04:59 +0200

    Première sauvegarde

---
+++ scripts/project2016.js
@@ -0,0 +1,13 @@
+// global variable for the project
+// the number of the selected news displaid in #selectedNews
+var selectedNewsNum = -1;
+// the list of the numbers of kept news, diplaid in #favList
+var favNewsList = [];
+
+
+
+// YOUR NAME HERE
+
+// YOUR CODE BELOW
+
+	
