3b957f4c859b54341056f001f94f970fe406a044
acm363 <atchedjichurchill@gmail.com>
2020-06-24 18:36:09 +0200

    Modification fichier html et js

--- scripts/project2016.js
+++ scripts/project2016.js
@@ -1,12 +1,12 @@
 // global variable for the project
 // the number of the selected news displaid in #selectedNews
 var selectedNewsNum = -1;
-// the list of the numbers of kept news, diplaid in #favList
+// the list of the numbers of kept news, displaid in #favList
 var favNewsList = [];
 
 
 
-// YOUR NAME HERE
+// ATCHEDJI Churchill
 
 // YOUR CODE BELOW
 
